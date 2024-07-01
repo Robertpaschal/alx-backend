ca5d5e5392b91533044735c350052f9b4e097099
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-01 20:22:07 +0100

    Babel configuration

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Flask application Module
+with Babel for internationalization
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class config:
+    """
+    Configuration class for the Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app: Flask = Flask(__name__)
+app.config.from_object(config)
+
+babel: Babel = Babel(app)
+
+@app.route('/')
+def index() -> str:
+    """
+    Renders the index.html template.
+
+    Returns:
+        The rendered HTML content of index.html.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
