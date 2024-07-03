2c495bc945259edc297f475a06174e43b671da55
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-03 02:47:20 +0100

    force locale configuration

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,7 +19,7 @@ class Config:
 app: Flask = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-babel: Babel  = Babel(app)
+babel: Babel = Babel(app)
 
 
 @babel.localeselector
